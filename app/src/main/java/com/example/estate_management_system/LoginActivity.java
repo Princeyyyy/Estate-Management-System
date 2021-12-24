f255840fdb0d97ceb58dbfdb9570de2c06b41313
Princeyyyy <princemumo80@gmail.com>
2021-12-24 12:28:02 +0300

    finished splash and login activities

---
+++ app/src/main/java/com/example/estate_management_system/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.estate_management_system;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginUsername)
+    EditText muserName;
+    @BindView(R.id.loginPassword)
+    EditText mloginPassword;
+    @BindView(R.id.loginButton)
+    Button mloginButton;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
+
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+
+        mloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = muserName.getText().toString().trim();
+                String password = mloginPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    muserName.setError("Username Required");
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mloginPassword.setError("Password Required");
+                } else {
+                    loader.setMessage("LogIn in Progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Login Failed " + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
